9cfca1e8a1cc6a759a495448e121b2e1c7073660
Goran <39734799+GoXYzz@users.noreply.github.com>
2022-02-07 18:09:17 +0100

    Add files via upload
    Make a file of speech

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ from pydub.silence import split_on_silence
 # create a speech recognition object
 r = sr.Recognizer()
 
+# Find short word on https://cloud.google.com/speech-to-text/docs/languages
+lang = input("Shortword for speaking language (blank for english):   ")
+dur = input("Duration in seconds:   ")
+file_name = input("File name:   ")
+
 with sr.Microphone() as source:
     # read the audio data from the default microphone
-    audio_data = r.record(source, duration=10) # Duration in seconds
+    audio_data = r.record(source, duration=int(dur)) 
     print("Recognizing...")
     # convert speech to text
-    # For additional languages, check the link https://developers.google.com/admin-sdk/directory/v1/languages
-    text = r.recognize_google(audio_data, language="sr-SR")
+    text = r.recognize_google(audio_data, language=lang)
     print(text)
+
+filename = file_name + '.txt'
+# writting speech into file 
+with open(filename, 'a') as thought:
+    thought.write('\n' + text.capitalize() + '.')
+
